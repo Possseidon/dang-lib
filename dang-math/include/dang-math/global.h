529c3f7013521d31831882cc7eae5df14eb29697
Unknown <xpossseidon@gmail.com>
2020-01-26 15:28:20 +0100

    Added more vector functionality.

--- dang-math/include/dang-math/global.h
+++ dang-math/include/dang-math/global.h
@@ -1,4 +1,26 @@
 #pragma once
+            
+#include <cmath>
+#include <algorithm>
+#include <array>         
 
-namespace dang::math {}
+namespace dang::math
+{
+
+constexpr double pi = 3.14159265358979323846;
+
+template <typename T>
+T radToDeg(T value)
+{
+    return value * T(180) / T(pi);
+}
+
+template <typename T>
+T degToRad(T value)
+{
+    return value * T(pi) / T(180);
+}
+
+}
+
 namespace dmath = dang::math;
--- dang-math/include/dang-math/vector.h
+++ dang-math/include/dang-math/vector.h
@@ -1,9 +1,5 @@
 #pragma once
 
-#include <cstddef>
-#include <tuple>
-#include <array>
-
 #include "global.h"
 
 namespace dang::math
@@ -89,6 +85,153 @@ struct VectorBase : protected std::array<T, Dim> {
         return other.unary([scalar](T a) { return scalar / a; });
     }
 
+    inline constexpr bool operator==(const Vector<T, Dim>& other) const
+    {
+        return all(other, [](T a, T b) { return a == b; });
+    }
+
+    static friend inline constexpr bool operator==(T scalar, const Vector<T, Dim>& other)
+    {
+        return all([scalar](T a) { return scalar == a; });
+    }
+
+    inline constexpr bool operator!=(const Vector<T, Dim>& other) const
+    {
+        return any(other, [](T a, T b) { return a != b; });
+    }
+
+    static friend inline constexpr bool operator!=(T scalar, const Vector<T, Dim>& other)
+    {
+        return any([scalar](T a) { return scalar != a; });
+    }
+
+    inline constexpr bool operator<(const Vector<T, Dim>& other) const
+    {
+        return all(other, [](T a, T b) { return a < b; });
+    }
+
+    static friend inline constexpr bool operator<(T scalar, const Vector<T, Dim>& other)
+    {
+        return all([scalar](T a) { return scalar < a; });
+    }
+
+    inline constexpr bool operator<=(const Vector<T, Dim>& other) const
+    {
+        return all(other, [](T a, T b) { return a <= b; });
+    }
+
+    static friend inline constexpr bool operator<=(T scalar, const Vector<T, Dim>& other)
+    {
+        return all([scalar](T a) { return scalar <= a; });
+    }
+
+    inline constexpr bool operator>(const Vector<T, Dim>& other) const
+    {
+        return all(other, [](T a, T b) { return a > b; });
+    }
+
+    static friend inline constexpr bool operator>(T scalar, const Vector<T, Dim>& other)
+    {
+        return all([scalar](T a) { return scalar > a; });
+    }
+
+    inline constexpr bool operator>=(const Vector<T, Dim>& other) const
+    {
+        return all(other, [](T a, T b) { return a >= b; });
+    }
+
+    static friend inline constexpr bool operator>=(T scalar, const Vector<T, Dim>& other)
+    {
+        return all([scalar](T a) { return scalar >= a; });
+    }
+
+    inline constexpr T sum() const
+    {
+        T result = T();
+        for (std::size_t i = 0; i < Dim; i++)
+            result += (*this)[i];
+        return result;
+    }
+
+    inline constexpr T dot(const Vector<T, Dim>& other) const
+    {
+        return ((*this) * other).sum();
+    }
+
+    inline constexpr T sqrdot() const
+    {
+        return ((*this) * (*this)).sum();
+    }
+
+    inline constexpr T length() const
+    {
+        static_assert(std::is_floating_point_v<T>, "vec::length requires a floating point type");
+        return std::sqrt(sqrdot());
+    }
+
+    inline constexpr Vector<T, Dim> normalized() const
+    {
+        static_assert(std::is_floating_point_v<T>, "vec::normalized requires a floating point type");
+        return (*this) / length();
+    }
+
+    inline constexpr Vector<T, Dim> vectorTo(const Vector<T, Dim>& other) const
+    {
+        return other - *this;
+    }
+
+    inline constexpr Vector<T, Dim> distanceTo(const Vector<T, Dim>& other) const
+    {
+        return other - *this;
+    }
+
+    inline constexpr T cosAngleTo(const Vector<T, Dim>& other) const
+    {
+        return std::clamp(dot(other) / (length() * other.length()), T(-1), T(1));
+    }
+
+    inline constexpr T angleRadTo(const Vector<T, Dim>& other) const
+    {
+        return std::acos(cosAngleTo(other));
+    }
+
+    inline constexpr T angleTo(const Vector<T, Dim>& other) const
+    {
+        return radToDeg(angleRadTo(other));
+    }
+
+    inline constexpr Vector<T, Dim> abs() const
+    {
+        return unary([](T a) { return a < T(0) ? -a : a; });
+    }
+
+    inline constexpr Vector<T, Dim> floor() const
+    {
+        static_assert(std::is_floating_point_v<T>, "vec::floor requires a floating point type");
+        return unary([](T a) { return std::floor(a); });
+    }
+
+    inline constexpr Vector<T, Dim> ceil() const
+    {
+        static_assert(std::is_floating_point_v<T>, "vec::ceil requires a floating point type");
+        return unary([](T a) { return std::ceil(a); });
+    }
+
+    inline constexpr Vector<T, Dim> min(const Vector<T, Dim>& other) const
+    {
+        return binary(other, [](T a, T b) { return a < b ? a : b; });
+    }
+
+    inline constexpr Vector<T, Dim> max(const Vector<T, Dim>& other) const
+    {
+        return binary(other, [](T a, T b) { return a > b ? a : b; });
+    }
+
+    inline constexpr Vector<T, Dim> reflect(const Vector<T, Dim>& normal) const
+    {
+        return *this - 2 * dot(normal) * normal;
+    }
+
     template <typename TTarget>
     explicit inline constexpr operator Vector<TTarget, Dim>()
     {
@@ -109,11 +252,47 @@ private:
     }
 
     template <typename Op>
+    inline constexpr bool all(const Vector<T, Dim>& other, const Op& op) const
+    {
+        bool result = true;
+        for (std::size_t i = 0; i < Dim; i++)
+            result = result && op((*this)[i], other[i]);
+        return result;
+    }
+
+    template <typename Op>
     inline constexpr Vector<T, Dim> unary(const Op& op) const
     {
         Vector<T, Dim> result;
         for (std::size_t i = 0; i < Dim; i++)
             result[i] = op((*this)[i]);
+        return result;
+    }
+
+    template <typename Op>
+    inline constexpr bool any(const Vector<T, Dim>& other, const Op& op) const
+    {
+        bool result = false;
+        for (std::size_t i = 0; i < Dim; i++)
+            result = result || op((*this)[i], other[i]);
+        return result;
+    }
+
+    template <typename Op>
+    inline constexpr bool all(const Op& op) const
+    {
+        bool result = true;
+        for (std::size_t i = 0; i < Dim; i++)
+            result = result && op((*this)[i]);
+        return result;
+    }
+
+    template <typename Op>
+    inline constexpr bool any(const Op& op) const
+    {
+        bool result = false;
+        for (std::size_t i = 0; i < Dim; i++)
+            result = result || op((*this)[i]);
         return result;
     }
 };
@@ -146,10 +325,41 @@ struct Vector<T, 2> : public VectorBase<T, 2> {
     inline constexpr Vector(T value) : VectorBase<T, 2>({ value, value }) {}
     inline constexpr Vector(T x, T y) : VectorBase<T, 2>({ x, y }) {}
 
+    static inline constexpr Vector<T, 2> fromSlope(T slope)
+    {
+        return { 1, slope };
+    }
+
+    static inline constexpr Vector<T, 2> fromAngleRad(T radians)
+    {
+        return { std::cos(radians), std::sin(radians) };
+    }
+
+    static inline constexpr Vector<T, 2> fromAngle(T degrees)
+    {
+        return fromAngleRad(degToRad(degrees));
+    }
+
     inline constexpr T& x() { return std::get<0>(*this); }
     inline constexpr T x() const { return std::get<0>(*this); }
     inline constexpr T& y() { return std::get<1>(*this); }
     inline constexpr T y() const { return std::get<1>(*this); }
+
+    inline constexpr Vector<T, 2> cross()
+    {
+        return { -y(), x() };
+    }
+
+    inline constexpr T cross(const Vector<T, 3>& other)
+    {
+        return x() * other.y() - y() * x();
+    }
+
+    inline constexpr T slope()
+    {
+        static_assert(std::is_floating_point_v<T>, "vec2::slope requires a floating point type");
+        return y() / x();
+    }
 };
 
 template <typename T>
@@ -165,6 +375,13 @@ struct Vector<T, 3> : public VectorBase<T, 3> {
     inline constexpr T y() const { return std::get<1>(*this); }
     inline constexpr T& z() { return std::get<2>(*this); }
     inline constexpr T z() const { return std::get<2>(*this); }
+
+    inline constexpr Vector<T, 3> cross(const Vector<T, 3>& other)
+    {
+        return { (*this)[1] * other[2] - (*this)[2] * other[1],
+                 (*this)[2] * other[0] - (*this)[0] * other[2],
+                 (*this)[0] * other[1] - (*this)[1] * other[0] };
+    }
 };
 
 template <typename T>
