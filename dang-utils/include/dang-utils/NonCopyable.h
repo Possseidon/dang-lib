3b8019c66c3891a3e8fec3bfd82cbcfbe8067007
Unknown <xpossseidon@gmail.com>
2020-02-10 21:36:34 +0100

    Added a NonCopyable type to utils.

---
+++ dang-utils/include/dang-utils/NonCopyable.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace dang::utils
+{
+
+struct NonCopyable
+{
+    NonCopyable() = default;
+    NonCopyable(const NonCopyable&) = delete;
+    NonCopyable(NonCopyable&&) = default;
+                                                       
+    NonCopyable& operator=(const NonCopyable&) = delete;
+    NonCopyable& operator=(NonCopyable&&) = default;
+};
+
+}
