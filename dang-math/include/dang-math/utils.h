603e2f753c919e6c9c848b969a9dd247302473fb
Possseidon <xpossseidon@gmail.com>
2020-02-19 12:47:30 +0100

    Documented utils.h and made deg-rad conversion inline constexpr.

--- dang-math/include/dang-math/utils.h
+++ dang-math/include/dang-math/utils.h
@@ -7,16 +7,19 @@
 namespace dang::math
 {
 
+/// <summary>The value of Pi.</summary>
 constexpr double pi = 3.14159265358979323846;
 
+/// <summary>Converts from radians to degrees using pi.</summary>
 template <typename T>
-T radToDeg(T value)
+inline constexpr T radToDeg(T value)
 {
     return value * T(180) / T(pi);
 }
 
+/// <summary>Converts from degrees to radians using pi.</summary>
 template <typename T>
-T degToRad(T value)
+inline constexpr T degToRad(T value)
 {
     return value * T(pi) / T(180);
 }
