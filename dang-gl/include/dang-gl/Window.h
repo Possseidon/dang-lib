c1fd6a56b9e533eed6d69a634339f15d6af83709
Possseidon <xpossseidon@gmail.com>
2020-02-21 14:35:44 +0100

    Registered all GLFW window callbacks.

--- dang-gl/include/dang-gl/Window.h
+++ dang-gl/include/dang-gl/Window.h
@@ -39,6 +39,8 @@ public:
     Window(const WindowInfo& info = WindowInfo());
     ~Window();
 
+    static Window& fromUserPointer(GLFWwindow* window);
+
     GLFWwindow* handle();
 
     template <class TInfo>
@@ -58,6 +60,26 @@ public:
     dutils::Event<Window&> onRender;
 
 private:
+    void registerCallbacks();
+
+    static void charCallback(GLFWwindow* window_handle, unsigned int codepoint);
+    static void charModsCallback(GLFWwindow* window_handle, unsigned int codepoint, int mods);
+    static void cursorEnterCallback(GLFWwindow* window_handle, int entered);
+    static void cursorPosCallback(GLFWwindow* window_handle, double xpos, double ypos);
+    static void dropCallback(GLFWwindow* window_handle, int path_count, const char* paths[]);
+    static void framebufferSizeCallback(GLFWwindow* window_handle, int width, int height);
+    static void keyCallback(GLFWwindow* window_handle, int key, int scancode, int action, int mods);
+    static void mouseButtonCallback(GLFWwindow* window_handle, int button, int action, int mods);
+    static void scrollCallback(GLFWwindow* window_handle, double xoffset, double yoffset);
+    static void windowCloseCallback(GLFWwindow* window_handle);
+    static void windowContentScaleCallback(GLFWwindow* window_handle, float xscale, float yscale);
+    static void windowFocusCallback(GLFWwindow* window_handle, int focused);
+    static void windowIconifyCallback(GLFWwindow* window_handle, int iconified);
+    static void windowMaximizeCallback(GLFWwindow* window_handle, int maximized);
+    static void windowPosCallback(GLFWwindow* window_handle, int xpos, int ypos);
+    static void windowRefreshCallback(GLFWwindow* window_handle);
+    static void windowSizeCallback(GLFWwindow* window_handle, int width, int height);
+
     GLFWwindow* handle_;
     dutils::EnumArray<BindingPoint, std::unique_ptr<Binding>> bindings_;
 };
--- dang-gl/src/Window.cpp
+++ dang-gl/src/Window.cpp
@@ -63,6 +63,8 @@ GLFWwindow* WindowInfo::createWindow() const
 Window::Window(GLFWwindow* handle)
     : handle_(handle)
 {
+    glfwSetWindowUserPointer(handle, this);
+    registerCallbacks();
 }
 
 Window::Window(const WindowInfo& info)
@@ -73,6 +75,11 @@ Window::Window(const WindowInfo& info)
 Window::~Window()
 {
     glfwDestroyWindow(handle_);
+}
+
+Window& Window::fromUserPointer(GLFWwindow* window)
+{
+    return *static_cast<Window*>(glfwGetWindowUserPointer(window));
 }
 
 GLFWwindow* Window::handle()
@@ -117,6 +124,157 @@ void Window::run()
         update();
         render();
     }
+}
+
+void Window::registerCallbacks()
+{
+    glfwSetCharCallback(handle_, charCallback);
+    glfwSetCharModsCallback(handle_, charModsCallback);
+    glfwSetCursorEnterCallback(handle_, cursorEnterCallback);
+    glfwSetCursorPosCallback(handle_, cursorPosCallback);
+    glfwSetDropCallback(handle_, dropCallback);
+    glfwSetFramebufferSizeCallback(handle_, framebufferSizeCallback);
+    glfwSetKeyCallback(handle_, keyCallback);
+    glfwSetMouseButtonCallback(handle_, mouseButtonCallback);
+    glfwSetScrollCallback(handle_, scrollCallback);
+    glfwSetWindowCloseCallback(handle_, windowCloseCallback);
+    glfwSetWindowContentScaleCallback(handle_, windowContentScaleCallback);
+    glfwSetWindowFocusCallback(handle_, windowFocusCallback);
+    glfwSetWindowIconifyCallback(handle_, windowIconifyCallback);
+    glfwSetWindowMaximizeCallback(handle_, windowMaximizeCallback);
+    glfwSetWindowPosCallback(handle_, windowPosCallback);
+    glfwSetWindowRefreshCallback(handle_, windowRefreshCallback);
+    glfwSetWindowSizeCallback(handle_, windowSizeCallback);
+}
+
+void Window::charCallback(GLFWwindow* window_handle, unsigned int codepoint)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)codepoint;
+}
+
+void Window::charModsCallback(GLFWwindow* window_handle, unsigned int codepoint, int mods)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)codepoint;
+    (void)mods;
+}
+
+void Window::cursorEnterCallback(GLFWwindow* window_handle, int entered)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)entered;
+}
+
+void Window::cursorPosCallback(GLFWwindow* window_handle, double xpos, double ypos)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)xpos;
+    (void)ypos;
+}
+
+void Window::dropCallback(GLFWwindow* window_handle, int path_count, const char* paths[])
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)path_count;
+    (void)paths;
+}
+
+void Window::framebufferSizeCallback(GLFWwindow* window_handle, int width, int height)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)width;
+    (void)height;
+}
+
+void Window::keyCallback(GLFWwindow* window_handle, int key, int scancode, int action, int mods)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)key;
+    (void)scancode;
+    (void)action;
+    (void)mods;
+}
+
+void Window::mouseButtonCallback(GLFWwindow* window_handle, int button, int action, int mods)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)button;
+    (void)action;
+    (void)mods;
+}
+
+void Window::scrollCallback(GLFWwindow* window_handle, double xoffset, double yoffset)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)xoffset;
+    (void)yoffset;
+}
+
+void Window::windowCloseCallback(GLFWwindow* window_handle)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+}
+
+void Window::windowContentScaleCallback(GLFWwindow* window_handle, float xscale, float yscale)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)xscale;
+    (void)yscale;
+}
+
+void Window::windowFocusCallback(GLFWwindow* window_handle, int focused)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)focused;
+}
+
+void Window::windowIconifyCallback(GLFWwindow* window_handle, int iconified)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)iconified;
+}
+
+void Window::windowMaximizeCallback(GLFWwindow* window_handle, int maximized)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)maximized;
+}
+
+void Window::windowPosCallback(GLFWwindow* window_handle, int xpos, int ypos)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)xpos;
+    (void)ypos;
+}
+
+void Window::windowRefreshCallback(GLFWwindow* window_handle)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+}
+
+void Window::windowSizeCallback(GLFWwindow* window_handle, int width, int height)
+{
+    Window& window = Window::fromUserPointer(window_handle);
+    (void)window;
+    (void)width;
+    (void)height;
 }
 
 }
