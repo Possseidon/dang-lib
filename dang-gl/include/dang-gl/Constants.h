a75236a1c8e07b2c31ceccc259912baabc77a865
Possseidon <xpossseidon@gmail.com>
2020-03-05 14:11:50 +0100

    Added a header with constants, useful vbo generation.

---
+++ dang-gl/include/dang-gl/Constants.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "dang-utils/enum.h"             
+#include "dang-math/enums.h"
+#include "dang-math/geometry.h"
+#include "dang-math/matrix.h"
+#include "dang-math/vector.h"
+
+namespace dang::gl
+{
+
+constexpr dutils::EnumArray<dmath::Facing3, dmath::Plane3> CubePlanes
+{
+    dmath::Plane3({ 0, 0, 0 }, dmath::mat2x3({{  0, 0,  1 }, { 0, 1,  0 }})),
+    dmath::Plane3({ 1, 0, 1 }, dmath::mat2x3({{  0, 0, -1 }, { 0, 1,  0 }})),
+    dmath::Plane3({ 0, 0, 0 }, dmath::mat2x3({{  1, 0,  0 }, { 0, 0,  1 }})),
+    dmath::Plane3({ 0, 1, 1 }, dmath::mat2x3({{  1, 0,  0 }, { 0, 0, -1 }})),
+    dmath::Plane3({ 1, 0, 0 }, dmath::mat2x3({{ -1, 0,  0 }, { 0, 1,  0 }})),
+    dmath::Plane3({ 0, 0, 1 }, dmath::mat2x3({{  1, 0,  0 }, { 0, 1,  0 }}))
+};
+
+constexpr std::array QuadTexCoords
+{
+    dmath::vec2(0, 0),
+    dmath::vec2(1, 0),
+    dmath::vec2(1, 1),
+    dmath::vec2(1, 1),
+    dmath::vec2(0, 1),
+    dmath::vec2(0, 0)
+};
+
+constexpr std::array CenteredTexCoords
+{
+    dmath::vec2(-1, -1),
+    dmath::vec2(+1, -1),
+    dmath::vec2(+1, +1),
+    dmath::vec2(+1, +1),
+    dmath::vec2(-1, +1),
+    dmath::vec2(-1, -1)
+};
+
+}
