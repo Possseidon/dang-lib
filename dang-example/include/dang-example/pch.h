92720e51ed0a1ade61e512a3feb9734c6281d11c
Unknown <xpossseidon@gmail.com>
2020-04-21 11:33:46 +0200

    Support for Visual Leak Detector when using DebugVLD as configuration.

--- dang-example/include/dang-example/pch.h
+++ dang-example/include/dang-example/pch.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#ifdef ENABLE_VLD
+// vld.h checks for either _DEBUG or VLD_FORCE_ENABLE
+// this way, ENABLE_VLD can also be used in non-debug builds
+#define VLD_FORCE_ENABLE
+#include <vld.h>
+#endif
+
 #include "dang-gl/pch.h"
 
 #include <iostream>
