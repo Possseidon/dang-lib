840b183cabf3ecca4f79d944e5abe2787cb597fd
Unknown <xpossseidon@gmail.com>
2020-05-08 23:43:13 +0200

    Added a bunch of = which fix VS code formatting.

--- dang-math/include/dang-math/consts.h
+++ dang-math/include/dang-math/consts.h
@@ -16,21 +16,18 @@ template <std::size_t Dim>
 constexpr dutils::EnumArray<Axis<Dim>, ivec<Dim>> AxisVector{};
 
 template <>
-constexpr dutils::EnumArray<Axis1, ivec1> AxisVector<1>
-{
+constexpr dutils::EnumArray<Axis1, ivec1> AxisVector<1> = {
     ivec1(1)
 };
 
 template <>
-constexpr dutils::EnumArray<Axis2, ivec2> AxisVector<2>
-{
+constexpr dutils::EnumArray<Axis2, ivec2> AxisVector<2> = {
     ivec2(1, 0),
     ivec2(0, 1)
 };
 
 template <>
-constexpr dutils::EnumArray<Axis3, ivec3> AxisVector<3>
-{
+constexpr dutils::EnumArray<Axis3, ivec3> AxisVector<3> = {
     ivec3(1, 0, 0),
     ivec3(0, 1, 0),
     ivec3(0, 0, 1)
@@ -46,15 +43,13 @@ template <std::size_t Dim>
 constexpr dutils::EnumArray<Facing<Dim>, ivec<Dim>> FacingVector{};
 
 template <>
-constexpr dutils::EnumArray<Facing1, ivec1> FacingVector<1>
-{
+constexpr dutils::EnumArray<Facing1, ivec1> FacingVector<1> = {
     ivec1(-1),
     ivec1(+1)
 };
 
 template <>
-constexpr dutils::EnumArray<Facing2, ivec2> FacingVector<2>
-{
+constexpr dutils::EnumArray<Facing2, ivec2> FacingVector<2> = {
     ivec2(-1, 0),
     ivec2(+1, 0),
     ivec2(0, -1),
@@ -62,8 +57,7 @@ constexpr dutils::EnumArray<Facing2, ivec2> FacingVector<2>
 };
 
 template <>
-constexpr dutils::EnumArray<Facing3, ivec3> FacingVector<3>
-{
+constexpr dutils::EnumArray<Facing3, ivec3> FacingVector<3> = {
     ivec3(-1, 0, 0),
     ivec3(+1, 0, 0),
     ivec3(0, -1, 0),
@@ -82,15 +76,13 @@ template <std::size_t Dim>
 constexpr dutils::EnumArray<Corner<Dim>, ivec<Dim>> CornerVector{};
 
 template <>
-constexpr dutils::EnumArray<Corner1, ivec1> CornerVector<1>
-{
+constexpr dutils::EnumArray<Corner1, ivec1> CornerVector<1> = {
     ivec1(0),
     ivec1(1)
 };
 
 template <>
-constexpr dutils::EnumArray<Corner2, ivec2> CornerVector<2>
-{
+constexpr dutils::EnumArray<Corner2, ivec2> CornerVector<2> = {
     ivec2(0, 0),
     ivec2(1, 0),
     ivec2(0, 1),
@@ -98,8 +90,7 @@ constexpr dutils::EnumArray<Corner2, ivec2> CornerVector<2>
 };
 
 template <>
-constexpr dutils::EnumArray<Corner3, ivec3> CornerVector<3>
-{
+constexpr dutils::EnumArray<Corner3, ivec3> CornerVector<3> = {
     ivec3(0, 0, 0),
     ivec3(1, 0, 0),
     ivec3(0, 1, 0),
@@ -120,21 +111,18 @@ template <std::size_t Dim>
 constexpr dutils::EnumArray<Axis<Dim>, Facing<Dim>> AxisFacing{};
 
 template <>
-constexpr dutils::EnumArray<Axis1, Facing1> AxisFacing<1>
-{
+constexpr dutils::EnumArray<Axis1, Facing1> AxisFacing<1> = {
     Facing1::Right
 };
 
 template <>
-constexpr dutils::EnumArray<Axis2, Facing2> AxisFacing<2>
-{
+constexpr dutils::EnumArray<Axis2, Facing2> AxisFacing<2> = {
     Facing2::Right,
     Facing2::Up
 };
 
 template <>
-constexpr dutils::EnumArray<Axis3, Facing3> AxisFacing<3>
-{
+constexpr dutils::EnumArray<Axis3, Facing3> AxisFacing<3> = {
     Facing3::Right,
     Facing3::Up,
     Facing3::Front
@@ -150,15 +138,13 @@ template <std::size_t Dim>
 constexpr dutils::EnumArray<Facing<Dim>, Facing<Dim>> FacingFlipped{};
 
 template <>
-constexpr dutils::EnumArray<Facing1, Facing1> FacingFlipped<1>
-{
+constexpr dutils::EnumArray<Facing1, Facing1> FacingFlipped<1> = {
     Facing1::Right,
     Facing1::Left
 };
 
 template <>
-constexpr dutils::EnumArray<Facing2, Facing2> FacingFlipped<2>
-{
+constexpr dutils::EnumArray<Facing2, Facing2> FacingFlipped<2> = {
     Facing2::Right,
     Facing2::Left,
     Facing2::Up,
@@ -166,8 +152,7 @@ constexpr dutils::EnumArray<Facing2, Facing2> FacingFlipped<2>
 };
 
 template <>
-constexpr dutils::EnumArray<Facing3, Facing3> FacingFlipped<3>
-{
+constexpr dutils::EnumArray<Facing3, Facing3> FacingFlipped<3> = {
     Facing3::Right,
     Facing3::Left,
     Facing3::Up,
