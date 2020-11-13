017f0e0f9385e64d5fe7662c7c3a7d1c39b4cc74
Unknown <xpossseidon@gmail.com>
2020-11-13 17:46:04 +0100

    Implemented inv_exp using exp and made all lambdas constexpr.

--- dang-math/include/dang-math/interpolation.h
+++ dang-math/include/dang-math/interpolation.h
@@ -14,31 +14,28 @@ constexpr T interpolate(const T& from, const T& to, TFactor factor)
 namespace interp {
 
 /// <summary>Simple identity function.</summary>
-auto linear = [](auto x) { return x; };
+constexpr auto linear = [](auto x) { return x; };
 
 /// <summary>Starts slow, ends fast.</summary>
-auto quadratic = [](auto x) { return x * x; };
+constexpr auto quadratic = [](auto x) { return x * x; };
 /// <summary>Starts fast, ends slow.</summary>
-auto inv_quadratic = [](auto x) { return x * (2 - x); };
+constexpr auto inv_quadratic = [](auto x) { return x * (2 - x); };
 /// <summary>Starts slow, ends slow.</summary>
-auto cubic = [](auto x) { return x * x * (3 - 2 * x); };
+constexpr auto cubic = [](auto x) { return x * x * (3 - 2 * x); };
 
 /// <summary>True exponential interpolation, quadratic is faster and usually sufficient.</summary>
-auto exp = [](auto x) { return x * std::exp(x) / std::exp(1); };
+constexpr auto exp = [](auto x) { return x * std::exp(x) / std::exp(1); };
 /// <summary>True inverse-exponential interpolation, inv_quadratic is faster and usually sufficient.</summary>
-auto inv_exp = [](auto x) {
-    x = 1 - x;
-    return 1 - x * std::exp(x) / std::exp(1);
-};
+constexpr auto inv_exp = [](auto x) { return 1 - exp(1 - x); };
 /// <summary>True cosine interpolation, cubic is faster and usually sufficient.</summary>
-auto cosine = [](auto x) { return (1 - std::cos(x * static_cast<decltype(x)>(pi))) / 2; };
+constexpr auto cosine = [](auto x) { return (1 - std::cos(x * static_cast<decltype(x)>(pi))) / 2; };
 
 /// <summary>Alias for cubic.</summary>
-auto smooth = cubic;
+constexpr auto smooth = cubic;
 /// <summary>Alias for quadtratic.</summary>
-auto smooth_start = quadratic;
+constexpr auto smooth_start = quadratic;
 /// <summary>Alias for inv_quadtratic.</summary>
-auto smooth_end = inv_quadratic;
+constexpr auto smooth_end = inv_quadratic;
 
 } // namespace interp
 
