e6d7af5fbdc506b953c94ef612140f9983239afd
Unknown <xpossseidon@gmail.com>
2020-11-27 16:11:06 +0100

    Big switch to CMake.

---
+++ cmake/FindVLD.cmake
@@ -0,0 +1,36 @@
+if(MSVC)
+  option(WITH_VLD "With Visual Leak Detector")
+
+  if(WITH_VLD)
+    if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "4")
+      message("-- Enabling Visual Leak Detector for Win32")
+      find_library(VLD vld
+        HINTS
+          "$ENV{PROGRAMFILES\(x86\)}/Visual Leak Detector/lib/Win32"
+          "$ENV{PROGRAMFILES}/Visual Leak Detector/lib/Win32"
+      )
+    elseif("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+      message("-- Enabling Visual Leak Detector for Win64")
+      find_library(VLD vld
+        HINTS
+          "$ENV{PROGRAMFILES\(x86\)}/Visual Leak Detector/lib/Win64"
+          "$ENV{PROGRAMFILES}/Visual Leak Detector/lib/Win64"
+      )
+    else()
+      message(FATAL_ERROR "Only Win32 and Win64 support Visual Leak Detector.")
+    endif()
+
+    find_file(VLD_H vld.h
+      HINTS
+        "$ENV{PROGRAMFILES\(x86\)}/Visual Leak Detector/include"
+        "$ENV{PROGRAMFILES}/Visual Leak Detector/include"
+    )
+
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \"/FI${VLD_H}\"")
+
+    get_filename_component(VLD_LIBRARY_DIR "${VLD}" PATH)
+    link_directories("${VLD_LIBRARY_DIR}")
+
+    add_compile_definitions(VLD_FORCE_ENABLE)
+  endif()
+endif()
--- dang-example/include/dang-example/pch.h
+++ dang-example/include/dang-example/pch.h
@@ -1,12 +1,5 @@
 #pragma once
 
-#ifdef ENABLE_VLD
-// vld.h checks for either _DEBUG or VLD_FORCE_ENABLE
-// this way, ENABLE_VLD can also be used in non-debug builds
-#define VLD_FORCE_ENABLE
-#include <vld.h>
-#endif
-
 #include <iostream>
 
 #include "dang-gl/pch.h"
