35ab9c09c88a31f5e5f7592134a0095a48357566
Unknown <xpossseidon@gmail.com>
2020-12-04 22:25:19 +0100

    Added dutils::Overloaded, a common utility for std::visit.

--- dang-utils/include/dang-utils/utils.h
+++ dang-utils/include/dang-utils/utils.h
@@ -1,4 +1,15 @@
 #pragma once
 
-namespace dang::utils {}
+namespace dang::utils {
+
+template <typename... TFunctions>
+struct Overloaded : TFunctions... {
+    using TFunctions::operator()...;
+};
+
+template <typename... TFunctions>
+Overloaded(TFunctions...) -> Overloaded<TFunctions...>;
+
+} // namespace dang::utils
+
 namespace dutils = dang::utils;
