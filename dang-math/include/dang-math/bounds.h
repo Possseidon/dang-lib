38ac287d97386400f4afacc5a1b4d942def8b29b
Possseidon <xpossseidon@gmail.com>
2021-01-22 20:02:00 +0100

    Applied new naming conventions to dmath bounds.h.

--- dang-math/include/dang-math/bounds.h
+++ dang-math/include/dang-math/bounds.h
@@ -37,33 +37,33 @@ static constexpr T floormod(T numerator, T denominator)
 
 } // namespace detail
 
-template <typename T, std::size_t Dim>
+template <typename T, std::size_t VDim>
 struct Bounds;
 
 /// @brief An iterator, allowing iteration of any-dimensional integral bounds.
 /// @remark The last vector component iterates first, followed by the second to last, etc...
 /// @remark This results in better caching for the common use-case of iterating an array[x][y][z]
-template <typename T, std::size_t Dim>
+template <typename T, std::size_t VDim>
 struct BoundsIterator {
     static_assert(std::is_integral_v<T>, "BoundsIterator can only be used with integral types");
 
     using iterator_category = std::forward_iterator_tag;
-    using value_type = Vector<T, Dim>;
+    using value_type = Vector<T, VDim>;
     using difference_type = std::ptrdiff_t;
-    using pointer = Vector<T, Dim>*;
-    using reference = Vector<T, Dim>&;
+    using pointer = Vector<T, VDim>*;
+    using reference = Vector<T, VDim>&;
 
     constexpr BoundsIterator() = default;
 
-    explicit constexpr BoundsIterator(Bounds<T, Dim> bounds, Vector<T, Dim> current)
+    explicit constexpr BoundsIterator(Bounds<T, VDim> bounds, Vector<T, VDim> current)
         : bounds_(bounds)
         , current_(current)
     {}
 
     constexpr BoundsIterator& operator++()
     {
-        current_[Dim - 1]++;
-        for (std::size_t d = Dim - 1; d != 0; d--) {
+        current_[VDim - 1]++;
+        for (std::size_t d = VDim - 1; d != 0; d--) {
             if (current_[d] < bounds_.high[d])
                 break;
             current_[d] = bounds_.low[d];
@@ -83,32 +83,32 @@ struct BoundsIterator {
 
     constexpr bool operator!=(const BoundsIterator& other) const { return current_ != other.current_; }
 
-    constexpr const Vector<T, Dim>& operator*() const { return current_; }
+    constexpr const Vector<T, VDim>& operator*() const { return current_; }
 
-    constexpr const Vector<T, Dim>* operator->() const { return &current_; }
+    constexpr const Vector<T, VDim>* operator->() const { return &current_; }
 
 private:
-    Bounds<T, Dim> bounds_;
-    Vector<T, Dim> current_;
+    Bounds<T, VDim> bounds_;
+    Vector<T, VDim> current_;
 };
 
 /// @brief Generic bounds with low and high values for any dimensional vectors.
 /// @remark Integral bounds are high exclusive: [low, high)
-template <typename T, std::size_t Dim>
+template <typename T, std::size_t VDim>
 struct Bounds {
-    Vector<T, Dim> low;
-    Vector<T, Dim> high;
+    Vector<T, VDim> low;
+    Vector<T, VDim> high;
 
     /// @brief Initializes the bounds with zero-vectors for low and high.
     constexpr Bounds() = default;
 
     /// @brief Initializes high with the given value and low with zero.
-    explicit constexpr Bounds(const Vector<T, Dim>& high)
+    explicit constexpr Bounds(const Vector<T, VDim>& high)
         : high(high)
     {}
 
     /// @brief Initializes low and high with the given values.
-    constexpr Bounds(const Vector<T, Dim>& low, const Vector<T, Dim>& high)
+    constexpr Bounds(const Vector<T, VDim>& low, const Vector<T, VDim>& high)
         : low(low)
         , high(high)
     {}
@@ -116,28 +116,28 @@ struct Bounds {
     /// @brief Provides simplified access for one-dimensional bounds.
     T& lowValue()
     {
-        static_assert(Dim == 1);
+        static_assert(VDim == 1);
         return low.x();
     }
 
     /// @brief Provides simplified access for one-dimensional bounds.
     constexpr T lowValue() const
     {
-        static_assert(Dim == 1);
+        static_assert(VDim == 1);
         return low.x();
     }
 
     /// @brief Provides simplified access for one-dimensional bounds.
     T& highValue()
     {
-        static_assert(Dim == 1);
+        static_assert(VDim == 1);
         return high.x();
     }
 
     /// @brief Provides simplified access for one-dimensional bounds.
     constexpr T highValue() const
     {
-        static_assert(Dim == 1);
+        static_assert(VDim == 1);
         return high.x();
     }
 
@@ -148,7 +148,7 @@ struct Bounds {
     constexpr Bounds normalize() const
     {
         Bounds result;
-        for (std::size_t i = 0; i < Dim; i++) {
+        for (std::size_t i = 0; i < VDim; i++) {
             if (low[i] <= high[i]) {
                 result.low[i] = low[i];
                 result.high[i] = high[i];
@@ -162,10 +162,10 @@ struct Bounds {
     }
 
     /// @brief Returns the size of the bounds, which is equal to <c>high - low</c>.
-    constexpr Vector<T, Dim> size() const { return high - low; }
+    constexpr Vector<T, VDim> size() const { return high - low; }
 
     /// @brief Returns the center of the bounds, rounded down for integral types.
-    constexpr Vector<T, Dim> center() const
+    constexpr Vector<T, VDim> center() const
     {
         if constexpr (std::is_integral_v<T>)
             return (low + high - 1) / T(2);
@@ -181,21 +181,21 @@ struct Bounds {
 
     /// @brief Returns true, if other is enclosed by the calling bounds.
     /// @remark Comparison is inclusive for low and exclusive for high.
-    constexpr bool contains(const Vector<T, Dim>& vector) const
+    constexpr bool contains(const Vector<T, VDim>& vector) const
     {
         return vector.allGreaterEqual(low) && vector.allLess(high);
     }
 
     /// @brief Returns true, if other is enclosed by the calling bounds.
     /// @remark Comparison is inclusive for both low and high.
-    constexpr bool containsInclusive(const Vector<T, Dim>& vector) const
+    constexpr bool containsInclusive(const Vector<T, VDim>& vector) const
     {
         return vector.allGreaterEqual(low) && vector.allLessEqual(high);
     }
 
     /// @brief Returns true, if other is enclosed by the calling bounds.
     /// @remark Comparison is exclusive for both low and high.
-    constexpr bool containsExclusive(const Vector<T, Dim>& vector) const
+    constexpr bool containsExclusive(const Vector<T, VDim>& vector) const
     {
         return vector.allGreater(low) && vector.allLess(high);
     }
@@ -205,7 +205,7 @@ struct Bounds {
 
     /// @brief Clamps the given point into the calling bounds.
     /// @remark For integral types, high is clamped exclusive.
-    constexpr Vector<T, Dim> clamp(const Vector<T, Dim>& vector) const
+    constexpr Vector<T, VDim> clamp(const Vector<T, VDim>& vector) const
     {
         if constexpr (std::is_integral_v<T>)
             return vector.max(low).min(high - 1);
@@ -214,29 +214,29 @@ struct Bounds {
     }
 
     /// @brief Returns the result of a symmetrical modulus on the given vector.
-    constexpr Vector<T, Dim> mod(Vector<T, Dim> vector) const
+    constexpr Vector<T, VDim> mod(Vector<T, VDim> vector) const
     {
-        for (std::size_t i = 0; i < Dim; i++)
+        for (std::size_t i = 0; i < VDim; i++)
             vector[i] = low[i] + detail::floormod(vector[i] - low[i], high[i] - low[i]);
         return vector;
     }
 
     /// @brief Returns the bounds offset by the given amount.
-    constexpr Bounds offset(Vector<T, Dim> amount) const { return {low + amount, high + amount}; }
+    constexpr Bounds offset(Vector<T, VDim> amount) const { return {low + amount, high + amount}; }
 
     /// @brief Returns the bounds outset by the given amount.
-    constexpr Bounds outset(Vector<T, Dim> amount) const { return {low - amount, high + amount}; }
+    constexpr Bounds outset(Vector<T, VDim> amount) const { return {low - amount, high + amount}; }
 
     /// @brief Returns the bounds inset by the given amount.
-    constexpr Bounds inset(Vector<T, Dim> amount) const { return {low + amount, high - amount}; }
+    constexpr Bounds inset(Vector<T, VDim> amount) const { return {low + amount, high - amount}; }
 
     /// @brief Returns an enum-array, mapping corners to the actual positions of the corners.
-    constexpr dutils::EnumArray<Corner<Dim>, Vector<T, Dim>> corners() const
+    constexpr dutils::EnumArray<Corner<VDim>, Vector<T, VDim>> corners() const
     {
-        static_assert(Dim >= 1 && Dim <= 3);
-        dutils::EnumArray<Corner<Dim>, Vector<T, Dim>> result;
-        for (auto corner : Corner<Dim>())
-            result[corner] = low + Vector<T, Dim>(CornerVector<Dim>[corner]) * (high - low);
+        static_assert(VDim >= 1 && VDim <= 3);
+        dutils::EnumArray<Corner<VDim>, Vector<T, VDim>> result;
+        for (auto corner : Corner<VDim>())
+            result[corner] = low + Vector<T, VDim>(CornerVector<VDim>[corner]) * (high - low);
         return result;
     }
 
@@ -265,26 +265,26 @@ struct Bounds {
     friend constexpr bool operator>=(const Bounds& lhs, const Bounds& rhs) { return lhs.low.allGreaterEqual(rhs.high); }
 
     /// @brief Returns a bounds-iterator, allowing for range-based iteration.
-    constexpr BoundsIterator<T, Dim> begin() const { return BoundsIterator<T, Dim>(*this, low); }
+    constexpr BoundsIterator<T, VDim> begin() const { return BoundsIterator<T, VDim>(*this, low); }
 
     /// @brief Returns a bounds-iterator, allowing for range-based iteration.
-    constexpr BoundsIterator<T, Dim> end() const { return ++BoundsIterator<T, Dim>(*this, high - 1); }
+    constexpr BoundsIterator<T, VDim> end() const { return ++BoundsIterator<T, VDim>(*this, high - 1); }
 };
 
-template <std::size_t Dim>
-using bounds = Bounds<float, Dim>;
+template <std::size_t VDim>
+using bounds = Bounds<float, VDim>;
 
-template <std::size_t Dim>
-using dbounds = Bounds<double, Dim>;
+template <std::size_t VDim>
+using dbounds = Bounds<double, VDim>;
 
-template <std::size_t Dim>
-using ibounds = Bounds<int, Dim>;
+template <std::size_t VDim>
+using ibounds = Bounds<int, VDim>;
 
-template <std::size_t Dim>
-using ubounds = Bounds<unsigned, Dim>;
+template <std::size_t VDim>
+using ubounds = Bounds<unsigned, VDim>;
 
-template <std::size_t Dim>
-using sbounds = Bounds<std::size_t, Dim>;
+template <std::size_t VDim>
+using sbounds = Bounds<std::size_t, VDim>;
 
 using bounds1 = bounds<1>;
 using bounds2 = bounds<2>;
