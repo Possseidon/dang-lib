fb3c0d6252614d68e5ed1fd0a74b2755106eca8d
Possseidon <xpossseidon@gmail.com>
2021-03-04 22:02:13 +0100

    Added always_false helper to dang-utils.

--- dang-utils/include/dang-utils/utils.h
+++ dang-utils/include/dang-utils/utils.h
@@ -4,6 +4,21 @@
 
 namespace dang::utils {
 
+namespace detail {
+
+struct AlwaysFalseHelper {};
+
+} // namespace detail
+
+template <typename...>
+struct always_false : std::false_type {};
+
+template <>
+struct always_false<detail::AlwaysFalseHelper> : std::true_type {};
+
+template <typename... TArgs>
+inline constexpr auto always_false_v = always_false<TArgs...>::value;
+
 template <auto v>
 struct constant : std::integral_constant<decltype(v), v> {};
 
