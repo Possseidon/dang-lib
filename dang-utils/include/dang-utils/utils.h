0d56930023504912c95566d3a8780d57f23314fa
Possseidon <xpossseidon@gmail.com>
2021-03-10 17:32:23 +0100

    Added a bunch of type traits to check for existence of comparisons.

--- dang-utils/include/dang-utils/utils.h
+++ dang-utils/include/dang-utils/utils.h
@@ -30,4 +30,66 @@ struct Overloaded : TFunctions... {
 template <typename... TFunctions>
 Overloaded(TFunctions...) -> Overloaded<TFunctions...>;
 
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_equal_to_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_equal_to_comparable<TLeft, TRight, std::void_t<decltype(std::declval<TLeft>() == std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_equal_to_comparable_v = is_equal_to_comparable<TLeft, TRight>::value;
+
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_not_equal_to_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_not_equal_to_comparable<TLeft, TRight, std::void_t<decltype(std::declval<TLeft>() != std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_not_equal_to_comparable_v = is_not_equal_to_comparable<TLeft, TRight>::value;
+
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_less_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_less_comparable<TLeft, TRight, std::void_t<decltype(std::declval<TLeft>() < std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_less_comparable_v = is_less_comparable<TLeft, TRight>::value;
+
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_less_equal_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_less_equal_comparable<TLeft, TRight, std::void_t<decltype(std::declval<TLeft>() <= std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_less_equal_comparable_v = is_less_equal_comparable<TLeft, TRight>::value;
+
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_greater_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_greater_comparable<TLeft, TRight, std::void_t<decltype(std::declval<TLeft>() > std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_greater_comparable_v = is_greater_comparable<TLeft, TRight>::value;
+
+template <typename TLeft, typename TRight = TLeft, typename = void>
+struct is_greater_equal_comparable : std::false_type {};
+
+template <typename TLeft, typename TRight>
+struct is_greater_equal_comparable<TLeft,
+                                   TRight,
+                                   std::void_t<decltype(std::declval<TLeft>() >= std::declval<TRight>())>>
+    : std::true_type {};
+
+template <typename TLeft, typename TRight = TLeft>
+inline constexpr auto is_greater_equal_comparable_v = is_greater_equal_comparable<TLeft, TRight>::value;
+
 } // namespace dang::utils
