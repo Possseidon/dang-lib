c5f15a95c1643fa1e50bef4f8907ddd160d7317e
Possseidon <xpossseidon@gmail.com>
2021-03-13 16:49:30 +0100

    Added PNGLoader unit test.

---
+++ dang-gl/tests/test-PNGLoader.cpp
@@ -0,0 +1,65 @@
+#include "dang-gl/Image/PNGLoader.h"
+
+#include "catch2/catch.hpp"
+
+namespace dgl = dang::gl;
+namespace fs = std::filesystem;
+
+TEMPLATE_TEST_CASE_SIG("PNGLoader can read .png images in any format.",
+                       "[image]",
+                       ((dgl::PixelFormat format), format),
+                       dgl::PixelFormat::RED,
+                       dgl::PixelFormat::RG,
+                       dgl::PixelFormat::RGB,
+                       dgl::PixelFormat::BGR,
+                       dgl::PixelFormat::RGBA,
+                       dgl::PixelFormat::BGRA,
+                       dgl::PixelFormat::RED_INTEGER,
+                       dgl::PixelFormat::RG_INTEGER,
+                       dgl::PixelFormat::RGB_INTEGER,
+                       dgl::PixelFormat::BGR_INTEGER,
+                       dgl::PixelFormat::RGBA_INTEGER,
+                       dgl::PixelFormat::BGRA_INTEGER)
+{
+    auto flip = GENERATE(false, true);
+
+    for (const auto& entry : fs::directory_iterator("PngSuite")) {
+        if (!entry.is_regular_file())
+            continue;
+
+        const auto& path = entry.path();
+        if (path.extension() != ".png")
+            continue;
+
+        const auto& filename = path.filename().string();
+        auto should_fail = filename.find("x") == 0;
+
+        INFO("Loading " << filename << (should_fail ? " (should fail)" : ""))
+        CAPTURE(flip, format);
+
+        dgl::PNGLoader png_loader;
+
+        png_loader.onWarning.append([&](const dgl::PNGWarningInfo& info) {
+            CHECK(&info.image == &png_loader);
+            if (should_fail)
+                UNSCOPED_INFO(info.message);
+            else
+                WARN(info.message);
+        });
+
+        std::ifstream png_stream(path, std::ios::binary);
+
+        auto read_data = [&] {
+            png_loader.init(png_stream);
+            const auto& size = png_loader.size();
+            const auto& data = png_loader.read<format>(flip);
+            CAPTURE(size);
+            CHECK(data.size() == size.product());
+        };
+
+        if (should_fail)
+            CHECK_THROWS_AS(read_data(), dgl::PNGError);
+        else
+            CHECK_NOTHROW(read_data());
+    }
+}
