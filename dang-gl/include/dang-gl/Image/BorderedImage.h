31f28b89d5d7db0b4e4c7a462b462adfbfdeb852
Possseidon <xpossseidon@gmail.com>
2021-06-04 15:53:43 +0200

    Extracted ImageBorder into a separate header.
    If only ImageBorder is required it would drag Image with it as well.

--- dang-gl/include/dang-gl/Image/BorderedImage.h
+++ dang-gl/include/dang-gl/Image/BorderedImage.h
@@ -1,33 +1,12 @@
 #pragma once
 
 #include "dang-gl/Image/Image.h"
+#include "dang-gl/Image/ImageBorder.h"
 #include "dang-gl/Image/PixelFormat.h"
 #include "dang-gl/Image/PixelType.h"
 #include "dang-gl/global.h"
 
 namespace dang::gl {
-
-/// @brief Describes an image border that does nothing.
-struct ImageBorderNone {};
-
-/// @brief Describes an image border of a single solid color all around.
-template <PixelFormat v_pixel_format = PixelFormat::RGBA, PixelType v_pixel_type = PixelType::UNSIGNED_BYTE>
-struct ImageBorderSolid {
-    Pixel<v_pixel_format, v_pixel_type> color;
-};
-
-/// @brief Describes an image border, for which both sides are a copy from the opposite side.
-struct ImageBorderWrapBoth {};
-
-/// @brief Describes an image border, for which only the positive side is copied from the opposite negative sides.
-struct ImageBorderWrapPositive {};
-
-/// @brief A variant of the different image border styles.
-template <PixelFormat v_pixel_format = PixelFormat::RGBA, PixelType v_pixel_type = PixelType::UNSIGNED_BYTE>
-using ImageBorder = std::variant<ImageBorderNone,
-                                 ImageBorderSolid<v_pixel_format, v_pixel_type>,
-                                 ImageBorderWrapBoth,
-                                 ImageBorderWrapPositive>;
 
 /// @brief Adds or replaces a border around an image.
 template <std::size_t v_dim,
