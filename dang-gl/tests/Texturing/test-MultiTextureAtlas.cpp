84a5052c1c0cb31b99dbf2cb00562d512108856d
Possseidon <xpossseidon@gmail.com>
2021-08-30 22:53:24 +0200

    Implemented Texturing tests and restructured tests in general.
    - Added TextureAtlasUtils test.
      - Need an OpenGL context which mean no CI unfortunately.
      - Disabled using opengl label.
    - Added dang-* labels to all tests.
    - Added TextureAtlasTiles tests.

---
+++ dang-gl/tests/Texturing/test-MultiTextureAtlas.cpp
@@ -0,0 +1,5 @@
+#include "dang-gl/Texturing/MultiTextureAtlas.h"
+
+#include "catch2/catch.hpp"
+
+namespace dgl = dang::gl;
---
+++ dang-gl/tests/Texturing/test-TextureAtlasBase.cpp
@@ -0,0 +1,5 @@
+#include "dang-gl/Texturing/TextureAtlasBase.h"
+
+#include "catch2/catch.hpp"
+
+namespace dgl = dang::gl;
