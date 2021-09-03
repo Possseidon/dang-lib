f9ad82407dc58cccb94dbb2b5843ff1d452502df
Possseidon <xpossseidon@gmail.com>
2021-09-03 15:08:47 +0200

    Added tests for Convert<bool>.

--- dang-lua/tests/test-Convert.cpp
+++ dang-lua/tests/test-Convert.cpp
@@ -124,3 +124,71 @@ TEST_CASE("Convert supports pushing fail values.", "[lua][convert]")
         }
     }
 }
+
+TEST_CASE("Convert can work with booleans.", "[lua][convert]")
+{
+    using Convert = dlua::Convert<bool>;
+
+    SECTION("It has a push count of 1, allows nesting and is named 'boolean'.")
+    {
+        STATIC_REQUIRE(Convert::push_count == 1);
+        STATIC_REQUIRE(Convert::allow_nesting);
+        STATIC_REQUIRE(Convert::getPushTypename() == "boolean");
+    }
+    SECTION("Given a Lua state.")
+    {
+        LuaState lua;
+
+        SECTION("Convert::isExact returns true only for actual boolean values.")
+        {
+            CHECK_FALSE(Convert::isExact(*lua, 1));
+            lua_pushboolean(*lua, false);
+            CHECK(Convert::isExact(*lua, -1));
+            lua_pushboolean(*lua, true);
+            CHECK(Convert::isExact(*lua, -1));
+            lua_pushinteger(*lua, 42);
+            CHECK_FALSE(Convert::isExact(*lua, -1));
+        }
+        SECTION("Convert::isValid returns true for any value.")
+        {
+            CHECK(Convert::isValid(*lua, 1));
+            lua_pushboolean(*lua, false);
+            CHECK(Convert::isValid(*lua, -1));
+            lua_pushboolean(*lua, true);
+            CHECK(Convert::isValid(*lua, -1));
+            lua_pushinteger(*lua, 42);
+            CHECK(Convert::isValid(*lua, -1));
+        }
+        SECTION("Convert::at only returns false for false, nil and none and never returns std::nullopt.")
+        {
+            CHECK_FALSE(Convert::at(*lua, 1).value());
+            lua_pushnil(*lua);
+            CHECK_FALSE(Convert::at(*lua, -1).value());
+            lua_pushboolean(*lua, false);
+            CHECK_FALSE(Convert::at(*lua, -1).value());
+            lua_pushboolean(*lua, true);
+            CHECK(Convert::at(*lua, -1).value());
+            lua_pushinteger(*lua, 42);
+            CHECK(Convert::at(*lua, -1).value());
+        }
+        SECTION("Convert::check works the same as Convert::at and never throws.")
+        {
+            CHECK_FALSE(Convert::check(*lua, 1));
+            lua_pushnil(*lua);
+            CHECK_FALSE(Convert::check(*lua, -1));
+            lua_pushboolean(*lua, false);
+            CHECK_FALSE(Convert::check(*lua, -1));
+            lua_pushboolean(*lua, true);
+            CHECK(Convert::check(*lua, -1));
+            lua_pushinteger(*lua, 42);
+            CHECK(Convert::check(*lua, -1));
+        }
+        SECTION("Convert::push pushes a boolean value on the stack.")
+        {
+            Convert::push(*lua, false);
+            CHECK(lua_type(*lua, -1) == LUA_TBOOLEAN);
+            Convert::push(*lua, true);
+            CHECK(lua_type(*lua, -1) == LUA_TBOOLEAN);
+        }
+    }
+}
