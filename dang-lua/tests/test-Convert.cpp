c1643aafdc0275b5626435e7763bfd4abb58fa82
Possseidon <xpossseidon@gmail.com>
2021-09-05 19:19:51 +0200

    Add tests for all the different string Convert specializations.

--- dang-lua/tests/test-Convert.cpp
+++ dang-lua/tests/test-Convert.cpp
@@ -1309,6 +1309,230 @@ TEMPLATE_LIST_TEST_CASE("Convert can work with integers.", "[lua][convert][integ
 
 // --- Convert<string>
 
+using string_types = maybe_cref<std::string, std::string_view, const char*, char*, const char[5], char[5]>;
+TEMPLATE_LIST_TEST_CASE("Convert can work with strings.", "[lua][convert][string]", string_types)
+{
+    using Convert = dlua::Convert<TestType>;
+
+    SECTION("It has a push count of 1, allows nesting and is named 'string'.")
+    {
+        STATIC_REQUIRE(Convert::push_count == 1);
+        STATIC_REQUIRE(Convert::allow_nesting);
+        STATIC_REQUIRE(Convert::getPushTypename() == "string");
+    }
+}
+
+using validity_string_types = maybe_cref<std::string, std::string_view>;
+TEMPLATE_LIST_TEST_CASE("Convert check for validity of std::string and std::string_view.",
+                        "[lua][convert][string]",
+                        validity_string_types)
+{
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    SECTION("Convert::isExact returns true only for strings.")
+    {
+        CHECK_FALSE(Convert::isExact(*lua, 1));
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::isExact(*lua, -1));
+        lua_pushinteger(*lua, 42);
+        CHECK_FALSE(Convert::isExact(*lua, -1));
+        lua_pushboolean(*lua, true);
+        CHECK_FALSE(Convert::isExact(*lua, -1));
+    }
+    SECTION("Convert::isValid returns true for strings and numbers.")
+    {
+        CHECK_FALSE(Convert::isValid(*lua, 1));
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::isValid(*lua, -1));
+        lua_pushinteger(*lua, 42);
+        CHECK(Convert::isValid(*lua, -1));
+        UNSCOPED_INFO("The number is not converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TNUMBER);
+        lua_pushboolean(*lua, true);
+        CHECK_FALSE(Convert::isValid(*lua, -1));
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Convert can work with std::string.", "[lua][convert][string]", maybe_cref<std::string>)
+{
+    using namespace std::literals::string_literals;
+
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    SECTION("Convert::at returns valid strings and numbers and std::nullopt otherwise.")
+    {
+        CHECK(Convert::at(*lua, 1) == std::nullopt);
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::at(*lua, -1) == "test"s);
+        lua_pushinteger(*lua, 42);
+        CHECK(Convert::at(*lua, -1) == "42"s);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        lua_pushboolean(*lua, true);
+        CHECK(Convert::at(*lua, -1) == std::nullopt);
+    }
+    SECTION("Convert::check returns valid strings and numbers and throws a Lua error otherwise.")
+    {
+        CHECK(lua.shouldThrow([&] { Convert::check(*lua, 1); }) ==
+              "bad argument #1 to '?' (string expected, got no value)");
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::check(*lua, -1) == "test"s);
+        lua_pushinteger(*lua, 42);
+        CHECK(Convert::check(*lua, -1) == "42"s);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(lua.shouldThrow([&] {
+            lua_pushboolean(*lua, true);
+            Convert::check(*lua, 1);
+        }) == "bad argument #1 to '?' (string expected, got boolean)");
+    }
+    SECTION("Convert::push pushes a string on the stack.")
+    {
+        Convert::push(*lua, "test"s);
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(Convert::at(*lua, -1) == "test"s);
+    }
+    SECTION("Convert::push preserves embedded null-terminators.")
+    {
+        Convert::push(*lua, "\0te\0st\0"s);
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(Convert::at(*lua, -1) == "\0te\0st\0"s);
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Convert can work with std::string_view.",
+                        "[lua][convert][string]",
+                        maybe_cref<std::string_view>)
+{
+    using namespace std::literals::string_view_literals;
+
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    SECTION("Convert::at returns valid strings and numbers and std::nullopt otherwise.")
+    {
+        CHECK(Convert::at(*lua, 1) == std::nullopt);
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::at(*lua, -1) == "test"sv);
+        lua_pushinteger(*lua, 42);
+        CHECK(Convert::at(*lua, -1) == "42"sv);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        lua_pushboolean(*lua, true);
+        CHECK(Convert::at(*lua, -1) == std::nullopt);
+    }
+    SECTION("Convert::check returns valid strings and numbers and throws a Lua error otherwise.")
+    {
+        CHECK(lua.shouldThrow([&] { Convert::check(*lua, 1); }) ==
+              "bad argument #1 to '?' (string expected, got no value)");
+        lua_pushliteral(*lua, "test");
+        CHECK(Convert::check(*lua, -1) == "test"sv);
+        lua_pushinteger(*lua, 42);
+        CHECK(Convert::check(*lua, -1) == "42"sv);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(lua.shouldThrow([&] {
+            lua_pushboolean(*lua, true);
+            Convert::check(*lua, 1);
+        }) == "bad argument #1 to '?' (string expected, got boolean)");
+    }
+    SECTION("Convert::push pushes a string on the stack.")
+    {
+        Convert::push(*lua, "test"sv);
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(Convert::at(*lua, -1) == "test"sv);
+    }
+    SECTION("Convert::push preserves embedded null-terminators.")
+    {
+        Convert::push(*lua, "\0te\0st\0"sv);
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(Convert::at(*lua, -1) == "\0te\0st\0"sv);
+    }
+}
+
+using char_array_types = maybe_cref<const char[5], char[5]>;
+TEMPLATE_LIST_TEST_CASE("Convert can work with references to const char arrays.",
+                        "[lua][convert][string]",
+                        char_array_types)
+{
+    using namespace std::literals::string_literals;
+
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    SECTION("Convert::push trims of a potential null-terminator.")
+    {
+        Convert::push(*lua, "test");
+        CHECK(dlua::Convert<std::string>::at(*lua, -1) == "test"s);
+    }
+    SECTION("Convert::push preserves embedded null-terminators.")
+    {
+        const char value[5] = {'\0', 'a', '\0', '\0', 'e'};
+        Convert::push(*lua, value);
+        CHECK(dlua::Convert<std::string>::at(*lua, -1) == "\0a\0\0e"s);
+    }
+}
+
+TEMPLATE_LIST_TEST_CASE("Convert can work with C-Style strings.", "[lua][convert][string]", maybe_cref<const char*>)
+{
+    using namespace std::literals::string_literals;
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    auto is_valid = [](const char* str, const std::string& value) {
+        CHECKED_IF(str != nullptr) { CHECK(str == value); }
+    };
+
+    SECTION("Convert::at returns valid strings and numbers and std::nullopt otherwise.")
+    {
+        CHECK(Convert::at(*lua, 1) == std::nullopt);
+        lua_pushliteral(*lua, "test");
+        is_valid(Convert::at(*lua, -1).value(), "test"s);
+        lua_pushinteger(*lua, 42);
+        is_valid(Convert::at(*lua, -1).value(), "42"s);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        lua_pushboolean(*lua, true);
+        CHECK(Convert::at(*lua, -1) == std::nullopt);
+    }
+    SECTION("Convert::check returns valid string and numbers and throws a Lua error otherwise.")
+    {
+        CHECK(lua.shouldThrow([&] { Convert::check(*lua, 1); }) ==
+              "bad argument #1 to '?' (string expected, got no value)");
+        lua_pushliteral(*lua, "test");
+        is_valid(Convert::check(*lua, -1), "test"s);
+        lua_pushinteger(*lua, 42);
+        is_valid(Convert::check(*lua, -1), "42"s);
+        UNSCOPED_INFO("The number is converted to a string.");
+        CHECK(lua_type(*lua, -1) == LUA_TSTRING);
+        CHECK(lua.shouldThrow([&] {
+            lua_pushboolean(*lua, true);
+            Convert::check(*lua, 1);
+        }) == "bad argument #1 to '?' (string expected, got boolean)");
+    }
+}
+
+using cstyle_strings = maybe_cref<const char*, char*>;
+TEMPLATE_LIST_TEST_CASE("Convert can push C-Style strings.", "[lua][convert][string]", cstyle_strings)
+{
+    using Convert = dlua::Convert<TestType>;
+
+    LuaState lua;
+
+    SECTION("Convert::push pushes a string on the stack.")
+    {
+        Convert::push(*lua, "test");
+        CHECK(dlua::Convert<std::string>::at(*lua, -1) == "test");
+    }
+}
+
 // --- Convert<function>
 
 // --- Convert<optional>
