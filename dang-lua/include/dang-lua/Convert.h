8917864476a6dbd9e9ef65e263e119f68ed6c529
Possseidon <xpossseidon@gmail.com>
2022-02-23 09:54:05 +0100

    Add missing convert/Reference.h to Convert.h.

--- dang-lua/include/dang-lua/Convert.h
+++ dang-lua/include/dang-lua/Convert.h
@@ -13,6 +13,7 @@
 #include "dang-lua/convert/Nil.h"
 #include "dang-lua/convert/Number.h"
 #include "dang-lua/convert/Optional.h"
+#include "dang-lua/convert/Reference.h"
 #include "dang-lua/convert/State.h"
 #include "dang-lua/convert/String.h"
 #include "dang-lua/convert/Tuple.h"
