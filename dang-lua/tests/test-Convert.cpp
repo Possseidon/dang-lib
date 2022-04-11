686b3e1cffa7b4c442f2cd33f235786b2ceda2f0
Possseidon <xpossseidon@gmail.com>
2022-04-11 13:18:34 +0200

    Added basic test structure for dang-lua.

---
+++ dang-lua/tests/test-Convert.cpp
@@ -0,0 +1,5 @@
+#include "dang-lua/Convert.h"
+
+#include "catch2/catch.hpp"
+
+namespace dlua = dang::lua;
---
+++ dang-lua/tests/test-State.cpp
@@ -0,0 +1,5 @@
+#include "dang-lua/State.h"
+
+#include "catch2/catch.hpp"
+
+namespace dlua = dang::lua;
