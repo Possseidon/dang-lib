e268b8b101eb7a82901dbb74fd56220345868d5e
Possseidon <xpossseidon@gmail.com>
2022-06-04 20:48:52 +0200

    Have run-cmake-lint.bat return 1 in case of linting issues.

--- run-cmake-lint.bat
+++ run-cmake-lint.bat
@@ -1,13 +1,15 @@
 @echo off
 
-for %%f in (CMakeLists.tx? *.cmake) do cmake-lint --suppress-decorations "%%f"
+set ANY_ERRORS=0
+
+for %%f in (CMakeLists.tx? *.cmake) do cmake-lint --suppress-decorations "%%f" || set ANY_ERRORS=1
 
 call :lint_directory cmake
 for /D %%d in (dang-*) do call :lint_directory %%d
 call :lint_directory doc
 
-exit
+exit /b %ANY_ERRORS%
 
 :lint_directory
-for /R "%1" %%f in (CMakeLists.tx? *.cmake) do cmake-lint --suppress-decorations "%%f"
+for /R "%1" %%f in (CMakeLists.tx? *.cmake) do cmake-lint --suppress-decorations "%%f" || set ANY_ERRORS=1
 goto :eof
