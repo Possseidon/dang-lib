b0c3bf5e969132b72fc8d673342a452d9e50f894
Possseidon <xpossseidon@gmail.com>
2022-06-05 00:56:01 +0200

    This commit should be reverted by the pre-commit.ci bot.

--- dang-example/src/dang-example.cpp
+++ dang-example/src/dang-example.cpp
@@ -1,14 +1,18 @@
-#include "dang-example/global.h"
+   #include "dang-example/global.h"
+
+
+
 #include "dang-glfw/GLFW.h"
-#include "dang-glfw/Window.h"
+      #include "dang-glfw/Window.h"
 
-int main()
-{
-    dglfw::GLFW glfw;
+        int       main  (
+        ){dglfw::   GLFW   glfw;
 
-    dglfw::WindowInfo window_info;
-    window_info.title = "Hello World!";
+dglfw:: WindowInfo    window_info
+    ;
+     window_info.  title =   "Hello World!";
 
-    dglfw::Window window(window_info);
-    window.run();
-}
+   dglfw::Window window(
+        window_info
+);
+         window  .   run();}
